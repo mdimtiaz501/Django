abc378385df1b9aec830ca991a2fca5af471b67b
Md Imtiaz Chowdhury <39957822+mdimtiaz501@users.noreply.github.com>
2020-03-03 10:31:40 +0600

    Add files via upload

---
+++ CBVs basic and Template Views/CBVs/HelloWorld/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.views.generic import View, TemplateView
+from django.http import HttpResponse
+
+# Create your views here.
+#function based view
+'''def index(request):
+    return render(request,'index.html')'''
+    
+#class based view
+'''class CBView(View):
+    def get(self,request):
+        return HttpResponse("Hello world")'''
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['injection'] = 'CBVs'
+        return context
